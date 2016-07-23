6025fa1895feb4db309225a98bb42c0305776c95
Manuel Guerrero Fernandez <mguerrero@unknown.local>
2016-07-23 15:04:40 +0200

    Modelo News y Events. Añadidos modelos a la administracion de la pagina.

--- newspaper/news/models.py
+++ newspaper/news/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+#from newspaper.news.managers import BaseNewsManager
+
+
+@python_2_unicode_compatible
+class BaseNews(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    description = models.TextField(_('description'), blank=True, null=True)
+    publish_date = models.DateTimeField(_('publish date'))
+
+#    objects = BaseNewsManager()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class News(BaseNews):
+
+    class Meta:
+        verbose_name = _('news item')
+        verbose_name_plural = _('news')
+
+
+class Event(BaseNews):
+    start_date = models.DateTimeField(_('start date'))
+    end_date = models.DateTimeField(_('end date'))
+
+    class Meta:
+        verbose_name = _('event')
+        verbose_name_plural = _('events')
